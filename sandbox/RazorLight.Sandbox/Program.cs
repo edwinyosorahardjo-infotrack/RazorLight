0bd548955d6d2ac98a5f589b568cb569c4fd190f
Edwin Yosorahardjo <edwin.yosorahardjo@infotrack.com.au>
2019-05-10 13:58:14 +1000

    2.0.0-beta1.netstd2.infotrack

--- sandbox/RazorLight.Sandbox/Program.cs
+++ sandbox/RazorLight.Sandbox/Program.cs
@@ -3,25 +3,33 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RazorLight.Sandbox
 {
-	class Program
+    class Program
     {
-		public static async Task Main()
-		{
-			var engine = new RazorLightEngineBuilder()
-				.UseMemoryCachingProvider()
-				.UseEmbeddedResourcesProject(typeof(Program))
-				.Build();
+        public static async Task Main()
+        {
+            var engine = new RazorLightEngineBuilder()
+                .UseMemoryCachingProvider()
+                //				.UseEmbeddedResourcesProject(typeof(Program))
+                .Build();
 
-			string result = await engine.CompileRenderAsync("Views.Subfolder.A", null, null, null);
-			Console.WriteLine(result);
+            string template = "Hello, @Model.Name. Welcome to RazorLight repository";
+            var jsonModel =
+                @"{""Name"":""InfoTrack%20Template%20Service"",""Tasks"":[""Razor%20Engin"",""Jason%20Model"",""Cache%20Templates""],%20""Description"":%20""Product%20of%20InfoTrack%20Platform%20Team!""}";
+            var model = JsonConvert.DeserializeObject(jsonModel);
 
-			Console.WriteLine("Finished");
-		}
+            string result = await engine.CompileRenderAsync<string>("templateKey", jsonModel, null);
 
-		private static readonly object locker = new object();
+            //            string result = await engine.CompileRenderAsync<string>("Views.Subfolder.A", null, null);
+            Console.WriteLine(result);
+
+            Console.WriteLine("Finished");
+        }
+
+        private static readonly object locker = new object();
 
         private static int _j;
         public static int j
@@ -35,7 +43,7 @@ namespace RazorLight.Sandbox
             }
             set
             {
-                lock(locker)
+                lock (locker)
                 {
                     _j = value;
                 }
